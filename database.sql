b13bd34fbf01686b35f1516a0fc69bd62bd513ba
Bo-wei Chen <rayologist1002@gmail.com>
2021-09-03 02:14:41 +0800

    Update database.sql

--- database.sql
+++ database.sql
@@ -3,6 +3,9 @@
 -- /l -- list all db in psql
 -- /c -- switch connection to another db
 
+-- createdb todo_database
+-- psql todo_database
+
 CREATE DATABASE todo_database;
 
 -- \c into todo_database
