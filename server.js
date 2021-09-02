f3b5b1304eaeae35518688360eabe2064dc0d286
Bo-wei Chen <rayologist1002@gmail.com>
2021-09-03 02:02:58 +0800

    Init API

--- server.js
+++ server.js
@@ -2,3 +2,15 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 const pool = require("./database");
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    msg: "success",
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
